35ba92c89364486c8738d28fd1d1339c2661efe7
hli2020 <hongyangli2020@gmail.com>
2018-04-13 22:16:39 +0800

    udpate

--- script/base_1.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-DEVICE_ID=1
-CUDA_VISIBLE_DEVICES=$DEVICE_ID python main.py \
-    --device_id=$DEVICE_ID \
-    --phase=evaluate \
-    --model=logs/hyli_default_old20180327T0234/mask_rcnn_hyli_default_0030.pth \
-    --config=hyli_default_old
