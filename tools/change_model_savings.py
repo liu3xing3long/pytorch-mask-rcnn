23d5ac68772bec5d0238300d569d4c71a407f4e3
hli2020 <hongyangli2020@gmail.com>
2018-05-04 21:21:59 +0800

    update

--- tools/change_model_savings.py
+++ tools/change_model_savings.py
@@ -2,13 +2,14 @@ import torch
 import os
 
 # in terminal, execute python this_file_name.py, the following path is right; however, in MacPycharm, it sees tools folder as root
-base_name = 'results/meta_102_quick_1/train'
-file_name = 'mask_rcnn_ep_0006_iter_001237.pth'
+base_name = 'results/meta_101_quick_1/train'
+file_name = 'mask_rcnn_ep_0006_iter_001238.pth'
 # old file
 model_path = os.path.join(base_name, file_name)
 # load model
 checkpoints = torch.load(model_path)
 weights = checkpoints['state_dict']
+# CHANGE YOUR NEED HERE
 iter = checkpoints['iter'] - 1
 epoch = checkpoints['epoch']
 
