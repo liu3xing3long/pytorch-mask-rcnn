7c888047ed9dafab47349500d95f666f3e8ade06
hli2020 <hongyangli2020@gmail.com>
2018-05-08 01:00:52 +0800

    damn

--- script/base_102_new.sh
+++ script/base_102_new.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-DEVICE_ID=0,1,2,3,4,5,6,7
+DEVICE_ID=4,5,6,7
 
 CUDA_VISIBLE_DEVICES=$DEVICE_ID python main.py \
     --device_id=$DEVICE_ID \
     --phase=train \
     --debug=0 \
-    --config_name=base_102_new
+    --config_name=base_102_new \
+    TRAIN.BATCH_SIZE 8 \
+    TRAIN.FORCE_START_EPOCH 11 \
+
+
+
 
