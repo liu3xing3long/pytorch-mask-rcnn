b5279c5e48f9a58dfdd91d10fab756f2d6f4c500
hli2020 <hongyangli2020@gmail.com>
2018-05-15 00:36:54 +0800

    update

--- script/run_two_exps_sequentially.sh
+++ script/run_two_exps_sequentially.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
-sh script/base_4_gpu.sh 105/meta_105_quick_7.1 4,5,6,7
 
-sh script/base_4_gpu.sh 105/meta_105_quick_7.2 4,5,6,7
+DEVICE_ID=4,5,6,7
+CUDA_VISIBLE_DEVICES=$DEVICE_ID python main.py \
+    --device_id=$DEVICE_ID \
+    --phase=train \
+    --config_name=None \
+    --debug=0 \
+    --config_file=configs/105/meta_105_quick_7.1.yaml
+
+
+CUDA_VISIBLE_DEVICES=$DEVICE_ID python main.py \
+    --device_id=$DEVICE_ID \
+    --phase=train \
+    --config_name=None \
+    --debug=0 \
+    --config_file=configs/105/meta_105_quick_7.2.yaml
+
